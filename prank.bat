6e14fa41bc7fa53b263643aca36f7f30554816ed
Scott Nykl <scottnykl@gmail.com>
2024-03-26 14:17:56 -0400

    Moved popup.vbs generation to setup script and automated task scheduling.

--- prank.bat
+++ prank.bat
@@ -1,17 +1,3 @@
-rem Win + R   >>>   taskschd.msc
-rem Critical System Maintenance
-
 @echo off
-set VBScriptFile=popup.vbs
-(
-  echo Set objShell = CreateObject^("WScript.Shell"^)
-  echo countdownDuration = 10
-  echo For i = countdownDuration To 1 Step -1
-  echo     objShell.Popup "You've been pranked! Lock your computer next time... rebooting in " ^& i, 2, "Haha!!!", vbInformation + vbSystemModal
-  echo Next
-  rem echo objShell.Run "shutdown -r -t 0", 0, False
-  echo objShell.Popup "jk, not rebooting... but for real, lock your computer next time!", 5, "Haha!!!", vbInformation + vbSystemModal
-  echo objShell.Popup "Want this to go away? Hint: Critical System Maintenance", 10, "Haha!!!", vbInformation + vbSystemModal
-) > %VBScriptFile%
 start /min cmd /c "timeout /t 0 && wscript.exe "%~dp0popup.vbs""
 exit
---
+++ setup.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem create pop-up vbscript
+set VBScriptFile=popup.vbs
+(
+  echo Set objShell = CreateObject^("WScript.Shell"^)
+  echo countdownDuration = 10
+  echo For i = countdownDuration To 1 Step -1
+  echo     objShell.Popup "You've been pranked! Lock your computer next time... rebooting in " ^& i, 2, "Haha!!!", vbInformation + vbSystemModal
+  echo Next
+  rem echo objShell.Run "shutdown -r -t 0", 0, False
+  echo objShell.Popup "jk, not rebooting... but for real, lock your computer next time!", 5, "Haha!!!", vbInformation + vbSystemModal
+  echo objShell.Popup "Want this to go away? Hint: Critical System Maintenance", 10, "Haha!!!", vbInformation + vbSystemModal
+) > %VBScriptFile%
+
+rem setup system task to run the prank every 45 minutes
+for /f %%a in ('powershell -command "(Get-Date).ToString('MM/dd/yyyy')"') do set "today=%%a"
+for /f %%b in ('powershell -command "(Get-Date).ToString('HH:mm')"') do set "current_time=%%b"
+schtasks /create /tn "Critical System Maintenance" /tr "%cd%\prank.bat" /sc MINUTE /mo 30 /st %current_time% /sd %today%
